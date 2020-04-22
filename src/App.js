9b4f748da42e4f8be23839b12bc644d684ffea40
Asotikov Anton <asant@yandex.ru>
2020-04-22 22:00:33 +0300

    add task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PostProvider from './components/PostProvider';
+import Posts from './components/Posts';
+import NewPost from './components/NewPost';
+import ItemPost from './components/ItemPost';
+import ChangePost from './components/ChangePost';
 
 function App() {
+  const urlEnv = process.env.REACT_APP_URL;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PostProvider url={urlEnv}>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/posts/new" component={NewPost} />
+            <Route path="/posts/change/:id" component={ChangePost} />
+            <Route path="/posts/:id" component={ItemPost} />
+            <Route path="/" component={Posts} />
+          </Switch>
+        </div>
+      </Router>
+    </PostProvider>
   );
 }
 
